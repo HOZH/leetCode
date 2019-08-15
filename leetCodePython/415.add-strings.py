e048162ae6e0d52bc69dae6184ad784f34a7712b
HOZH <hong.zheng@stonybrook.edu>
2019-08-14 23:50:19 -0400

    daily update

---
+++ leetCodePython/415.add-strings.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=415 lang=python3
+#
+# [415] Add Strings
+#
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+
+        return str(int(num1)+int(num2))
+
