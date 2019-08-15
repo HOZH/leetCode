a28cf6f9f4eed48f1f5b329497600724afff795d
HOZH <hong.zheng@stonybrook.edu>
2019-08-15 01:28:28 -0400

    daily update

--- leetCodePython/989.add-to-array-form-of-integer.py
+++ leetCodePython/989.add-to-array-form-of-integer.py
@@ -3,12 +3,9 @@
 #
 # [989] Add to Array-Form of Integer
 #
+
+
 class Solution:
     def addToArrayForm(self, A: List[int], K: int) -> List[int]:
 
-
-        pass
-
-    
-        
-
+        return [int(i) for i in str(int("".join(list(map(lambda x: str(x), A))))+K)]
