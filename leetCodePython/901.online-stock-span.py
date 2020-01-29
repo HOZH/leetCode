868c52005de948d060bd5edfcffd846144206024
HOZH <hong.zheng@stonybrook.edu>
2020-01-29 14:44:00 -0500

    daily update

---
+++ leetCodePython/901.online-stock-span.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=901 lang=python3
+#
+# [901] Online Stock Span
+#
+
+# @lc code=start
+class StockSpanner:
+
+    def __init__(self):
+        
+
+    def next(self, price: int) -> int:
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
+# @lc code=end
+
--- leetCodePython/temp.py
+++ leetCodePython/temp.py
@@ -1,21 +1,40 @@
-#
-# @lc app=leetcode id=819 lang=python3
-#
-# [819] Most Common Word
-#
 from collections import Counter
 
 
 class Solution:
-    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+    def mostCommonWord(self, paragraph: str, banned):
         # gonna redo this question after I reviewed regular ex
         temp = paragraph.casefold().replace(',', ' ', -1).replace(';', ' ', -1).replace('\'', ' ', -1).replace('?', ' ', -1).replace('.',
                                                                                                                                      ' ', -1).replace('!', ' ', -1).split()
 
         temp = list(filter(lambda x: x not in banned, temp))
 
-        print(temp)
+        c = Counter(temp)
 
-        print(Counter(temp))
+        answer = []
 
-        return max(temp, key=temp.count)
+        print(c.most_common())
+
+        most = c.most_common()[0][1]
+
+        for i in range(len(c.most_common())):
+
+            if c.most_common()[i][1] ==most:
+
+
+                answer.append(c.most_common()[i][0])
+
+        return answer
+
+
+
+
+
+
+
+
+
+a = Solution()
+
+
+print(a.mostCommonWord("once upon upon once something",[]))
