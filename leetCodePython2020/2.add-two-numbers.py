e3e65c326492e7311bbf4643716239f9d34a52eb
HOZH <hong.zheng@stonybrook.edu>
2020-05-28 23:01:08 -0400

    start a new session

---
+++ leetCodePython2020/2.add-two-numbers.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=2 lang=python3
+#
+# [2] Add Two Numbers
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+# @lc code=end
+
