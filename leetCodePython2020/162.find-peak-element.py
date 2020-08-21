7a18f7fecfed6ba5a890b5a3107f5ebeaf4d8244
HOZH <hong.zheng@stonybrook.edu>
2020-08-21 15:04:13 -0400

    111/200

---
+++ leetCodePython2020/162.find-peak-element.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=162 lang=python3
+#
+# [162] Find Peak Element
+#
+
+# @lc code=start
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+
+        l, r = 0, len(nums)-1
+
+        while l < r:
+            m = l + (r-l)//2
+
+            if nums[m] > nums[m+1]:
+                r = m
+
+            else:
+                l = m+1
+        return l
+
+# @lc code=end
