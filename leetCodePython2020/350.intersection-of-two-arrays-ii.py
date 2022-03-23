31cb7ba9671ace8637e6710bdaa2910f2711fd53
hozh <hozh96@gmail.com>
2022-03-22 20:45:14 -0400

    507/600

--- leetCodePython2020/350.intersection-of-two-arrays-ii.py
+++ leetCodePython2020/350.intersection-of-two-arrays-ii.py
@@ -25,33 +25,4 @@ class Solution:
 
         return result
 
-
-class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        counter1 = {}
-        counter2 = {}
-        
-        def getCount(nums, counter):
-            for n in nums:
-                if n not in counter:
-                    counter[n] = 0
-                counter[n]+=1
-        
-        getCount(nums1, counter1)
-        getCount(nums2, counter2)
-        
-        intersections = {}
-        for n in counter2:
-            if n in counter1:
-                intersections[n] = min(counter1[n], counter2[n])
-        ans = []
-        for n in intersections:
-            while intersections[n]>0:
-                ans.append(n)
-                intersections[n]-=1
-        return ans
-
-
-
-
 # @lc code=end
