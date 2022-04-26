4f10b4591be044abce7e3dee832d10f092e42da8
hozh <hozh96@gmail.com>
2022-04-26 01:24:46 -0400

    520/600

---
+++ leetCodePython2020/2235.add-two-integers.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=2235 lang=python3
+#
+# [2235] Add Two Integers
+#
+
+# @lc code=start
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+        
+# @lc code=end
+
